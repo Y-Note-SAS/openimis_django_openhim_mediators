2452e232774bd548dac988810d89f7a709399725
rtchagang-vrai <r.tchagang@cloudfacile.com>
2026-09-19 07:59:18 +0100

    fix: uniy test test_get_forwards_request_and_returns_upstream_data

--- mediators/patient_mediator/tests.py
+++ mediators/patient_mediator/tests.py
@@ -64,7 +64,7 @@ class GetPatientTests(TestCase):
         args, kwargs = mock_request.call_args
         self.assertEqual(args[0], "GET")
         self.assertEqual(
-            args[1], "http://openimis.local:8080/api/api_fhir_r4/Patient"
+            args[1], "http://openimis.local:8080/api/api_fhir_r4/Patient/"
         )
         self.assertEqual(kwargs["params"], {})
         self.assertIn("Authorization", kwargs["headers"])
